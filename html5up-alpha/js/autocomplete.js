34e72eb66a665a57007a13f9a3c3c82c3fdeeca7
elvinali <ali.elvin@gmail.com>
2015-02-02 14:40:50 +0000

    Work in progress

--- html5up-alpha/js/autocomplete.js
+++ html5up-alpha/js/autocomplete.js
@@ -12,7 +12,6 @@ var placeSearchService, autocomplete, placesList;
 //};
 
 function initialize() {
-    alert("Hi Elvin");
     // Create the autocomplete object, restricting the search
     // to geographical location types.
     autocomplete = new google.maps.places.Autocomplete(
@@ -58,7 +57,6 @@ function debug() {
 // Bias the autocomplete object to the user's geographical location,
 // as supplied by the browser's 'navigator.geolocation' object.
 function geolocate() {
-    alert("Oh, hi again :)");
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             var geolocation = new google.maps.LatLng(
@@ -73,7 +71,6 @@ function geolocate() {
 }
 
 function findPlaces() {
-    //	alert("hello findPlaces");
     // prepare variables (filter)
     var type = "store"; //document.getElementById('gmap_type').value;
     var radius = 5000; //document.getElementById('gmap_radius').value;
