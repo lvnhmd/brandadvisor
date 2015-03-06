5ed55ffc20386e700d328b07d266c3207fa1e7d3
lvnhmd <ali.elvin@gmail.com>
2015-03-06 17:30:50 +0000

    Mean work in progress

--- MEAN/server.js
+++ MEAN/server.js
@@ -3,15 +3,12 @@ var bodyParser = require('body-parser')
 
 var app = express()
 app.use(bodyParser.json())
+app.use('/api/posts', require('./controllers/api/posts'))
+app.use('/', require('./controllers/static'))
+//app.use(require('./controllers/api/posts'))
 
-app.get('/api/posts', function (req, res) {
-  res.json([
-    {
-      username: 'dickeyxxx',
-      body: 'node rocks!'
-    }
-  ])
-})
+
+
 
 app.listen(3000, function () {
   console.log('Server listening on', 3000)
